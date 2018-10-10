a76ad22753d8c8fa8059152aa2a558a5e9e2ed98
VIERHELLER <jan-niklas.vierheller@lhsystems.com>
2018-10-10 16:15:50 +0200

    Initial commit

---
+++ routes/survey.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+
+router.get('/', function(req, res, next){
+  fs.readFile('voting.txt', (err, data) => {
+    if (err) throw err;
+
+    data = String(data);
+    var contents = data.split('');
+    console.log(data);
+
+    var countUp = contents.filter(function(x){return x=='+'}).length;
+    var countDown = contents.filter(function(x){return x=='-'}).length;
+
+    res.render('survey', { up: countUp, down: countDown });
+  });
+})
+
+router.post('/up', function(req, res, next) {
+  fs.appendFile('voting.txt', '+', (err) => {
+    if (err) throw err;
+
+    console.log('Someone voted up');
+    res.send('OK');
+  });
+});
+
+router.post('/down', function(req, res, next) {
+  fs.appendFile('voting.txt', '-', (err) => {
+    if (err) throw err;
+
+    console.log('Someone voted down');
+    res.send('OK');
+  });
+});
+
+module.exports = router;
