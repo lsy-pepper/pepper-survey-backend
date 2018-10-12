2d349395c9eaf7ed1cd2ceb649e6c6a75eae81f5
VIERHELLER <jan-niklas.vierheller@lhsystems.com>
2018-10-12 15:27:26 +0200

    Redis

--- routes/survey.js
+++ routes/survey.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var redis = require('redis');
+var client = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
+
+var voteKey = "vote";
 
 
 router.get('/', function(req, res, next){
-  fs.readFile('voting.txt', (err, data) => {
-    if (err) throw err;
-
-    data = String(data);
+  client.get(voteKey, function (err, reply) {
+    if(err) throw err;
+    data = reply.toString()
     var contents = data.split('');
     console.log(data);
 
@@ -19,18 +22,18 @@ router.get('/', function(req, res, next){
 })
 
 router.post('/up', function(req, res, next) {
-  fs.appendFile('voting.txt', '+', (err) => {
-    if (err) throw err;
-
+  client.get(voteKey, function (err, reply) {
+    if(err) throw err;
+    client.set(voteKey, reply.toString()+"+");
     console.log('Someone voted up');
     res.sendStatus(200);
   });
 });
 
 router.post('/down', function(req, res, next) {
-  fs.appendFile('voting.txt', '-', (err) => {
-    if (err) throw err;
-
+  client.get(voteKey, function (err, reply) {
+    if(err) throw err;
+    client.set(voteKey, reply.toString()+"-");
     console.log('Someone voted down');
     res.sendStatus(200);
   });
